f9e42f7efd916b38d833c735ed2a0e9bda500e48
DWL <DWL>
2020-11-03 16:12:40 +0900

    [ADD]Retofit Client and Urls

--- realworldandroidkotlin/app/src/main/java/com/example/realworld_android_kotlin/model/remote/RealWordAPIClient.kt
+++ realworldandroidkotlin/app/src/main/java/com/example/realworld_android_kotlin/model/remote/RealWordAPIClient.kt
@@ -1,2 +1,18 @@
 package com.example.realworld_android_kotlin.model.remote
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RealWordAPIClient {
+    private var instance : Retrofit? = null
+
+    fun getInstance() : Retrofit {
+        if (instance == null) {
+            instance = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        return instance!!
+    }
+}
--- realworldandroidkotlin/app/src/main/java/com/example/realworld_android_kotlin/model/remote/Urls.kt
+++ realworldandroidkotlin/app/src/main/java/com/example/realworld_android_kotlin/model/remote/Urls.kt
@@ -1,2 +1,15 @@
 package com.example.realworld_android_kotlin.model.remote
 
+const val BASE_URL = "https://conduit.productionready.io/api"
+const val AUTHENTICATION = "/api/users/login"
+const val REGISTRATION = "/api/users"
+const val USER = "/api/user"
+const val PROFILE = "/api/profiles/{username}"
+const val FOLLOW = "/api/profiles/{username}/follow"
+const val ARTICLE = "/api/articles/{slug}"
+const val ARTICLES = "/api/articles"
+const val FEED = "/api/articles/feed"
+const val COMMENTS = "/api/articles/{slug}/comments"
+const val DELETE_COMMENTS = "/api/articles/{slug}/comments/{id}"
+const val FAVORITE_ARTICLE = "/api/articles/{slug}/favorite"
+const val TAG = "/api/tags"
